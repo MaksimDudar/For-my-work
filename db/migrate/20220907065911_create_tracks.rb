3215af8e0777ce3c7e95b0c15dfd979ff9f60644
MaksimDudar <maksimdudar1@gmail.com>
2022-09-07 10:12:57 +0300

    add index and show list tracks

---
+++ db/migrate/20220907065911_create_tracks.rb
@@ -0,0 +1,16 @@
+class CreateTracks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tracks do |t|
+      t.string :pl
+      t.string :truck_by
+      t.string :driver_by
+      t.string :perecep
+      t.string :truck_rus
+      t.string :driver_rus
+      t.string :semi_trailer
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
