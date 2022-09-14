a20e5eecd9f6a582df3085671d85a90eee1c5144
MaksimDudar <maksimdudar1@gmail.com>
2022-09-14 22:42:21 +0300

    update2

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ApplicationRecord
   has_many :comments, dependent: :destroy
   def self.search(search)
-   where("truck_by LIKE ?", "%#{search}%")
+   where("truck_by LIKE ?", "%#{search}%").or where("truck_rus LIKE ?", "%#{search}%").or where("semi_trailer LIKE ?", "%#{search}%")
 end
 end
